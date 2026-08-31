23a2bcaf7d4b9f0ddac840f4fce375e3fe65602e
Francis Niog <francisniog205@gmail.com>
2026-08-31 22:40:17 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let login_page = document.querySelectorAll('login-page');
+let givenname = document.querySelectorAll('givenname');
+let middleinitial = document.querySelectorAll('middleinitial');
+let suffix = document.querySelectorAll('suffix');
+let program = document.querySelectorAll('program');
+let section = document.querySelectorAll('section');
+let loginbtn = document.querySelectorAll('login-btn');
+
